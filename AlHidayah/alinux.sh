22553591e619126dda14153ea34b5575160ac539
alfaqir <alfaqir@nabawiy.com>
2013-03-19 16:21:44 +0700

    update

--- AlHidayah/alinux.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-/usr/bin/rsync -avz --delete dhanuxe@ssi.alinux.web.id:~/www/alinux/ /home/deanet/www/alinux/
-/usr/bin/rsync -avz --delete /home/deanet/www/alinux/ deanet@nabawiy.com:~/www/alinux/
